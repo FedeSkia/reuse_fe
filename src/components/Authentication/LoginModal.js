d3a79f0d6ebf9eaa89f560a4f8457cedd25d2445
Fede <conoci.federico@gmail.com>
2021-04-18 22:39:16 +0200

    added navbar and image for login

--- src/components/Authentication/LoginModal.js
+++ src/components/Authentication/LoginModal.js
@@ -23,6 +23,7 @@ export default class LoginModal extends React.Component {
             .then(response => {
                 if (response.status === 200) {
                     this.setState({successfulLogin: true});
+                    this.props.closeLoginModal();
                 }
             })
             .catch((error) => {
@@ -60,7 +61,8 @@ export default class LoginModal extends React.Component {
                                           placeholder="Inserisci email"
                                           onChange={event => {
                                               this.setState({username: event.target.value})
-                                          }}/>
+                                          }}
+                                />
                             <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                             </Form.Text>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Col, Container, Image, Row} from "react-bootstrap";
+import {Button, Col, Container, Form, FormControl, Image, Nav, Navbar, NavDropdown, Row} from "react-bootstrap";
 import LoginModal from "./Authentication/LoginModal";
 
 
@@ -33,15 +33,29 @@ export default class Header extends React.Component {
 
     render() {
         return(
-            <Container>
-                <Row>
-                    <Col>1 of 3</Col>
-                    <Col xs={6}>2 of 3 (wider)</Col>
-                    <Col>
-                        <Image onClick={this.openLoginModal} src="login.png" fluid  />
-                    </Col>
-                </Row>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <div onClick={this.openLoginModal}>
+                        <span>
+                            Login
+                        </span>
+                        <Image className={"login-img"} src="login-image.png" fluid  />
+                    </div>
+                </Navbar.Collapse>
                 {this.renderLoginModal()}
-            </Container>);
+            </Navbar>)
     }
 }
